eafdda95e89e1cd2f95aa015cddee21c8e1d343a
AHMAD BWIDANI <ahmad2135@gmail.com>
2022-05-21 07:15:15 +0300

    Add Admin Theme Add dashboard controller

--- Appsuggest/App_Start/RouteConfig.cs
+++ Appsuggest/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace Appsuggest
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+           
         }
     }
 }
---
+++ Appsuggest/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Appsuggest.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index","AppPlatforms");
+        }
+
+    }
+}
