2175e1678dd7325dd82853644ec83381a2a59e5b
AHMAD BWIDANI <ahmad2135@gmail.com>
2022-05-27 11:20:05 +0300

    add apps fix images

--- Appsuggest/Controllers/HomeController.cs
+++ Appsuggest/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Appsuggest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,44 @@ namespace Appsuggest.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private appsuggestEntities1 db = new appsuggestEntities1();
 
+        public ActionResult Index(HomeSearchViewModel homeSearchViewModel = null)
+        {
+            IQueryable<App> apps = db.Apps;
+            if (homeSearchViewModel != null)
+            {
+                apps = apps.Where(a => homeSearchViewModel.AppTypeId == null || a.AppTypeId == homeSearchViewModel.AppTypeId)
+                    .Where(a => homeSearchViewModel.AppPlatformId == null || a.AppPlatformId == homeSearchViewModel.AppPlatformId)
+                    .Where(a => homeSearchViewModel.ProviderId == null || a.ProviderId == homeSearchViewModel.ProviderId)
+                    .Where(a => homeSearchViewModel.Search == null ||(
+                    a.Name.Contains(homeSearchViewModel.Search) || 
+                    a.Description.Contains(homeSearchViewModel.Search) || 
+                    a.Tags.Contains(homeSearchViewModel.Search)));
+            }
+            else { homeSearchViewModel = new HomeSearchViewModel(); }
+                homeSearchViewModel.Apps = apps.ToList();
+                homeSearchViewModel.AppPlatformList = GetAppPlatformList();
+                homeSearchViewModel.AppTypeList = GetAppTypeList();
+                homeSearchViewModel.ProviderList = GetProviderList();
+            
+            return View(homeSearchViewModel);
+        }
+        public List<SelectListItem> GetAppTypeList()
+        {
+            return db.AppTypes.Select(x =>
+            new SelectListItem { Text = x.Name, Value = x.Id.ToString() }).ToList();
+        } 
+        public List<SelectListItem> GetAppPlatformList()
+        {
+            return db.AppPlatforms.Select(x =>
+           new SelectListItem { Text = x.Name, Value = x.Id.ToString() }).ToList();
+        }
+        public List<SelectListItem> GetProviderList()
+        {
+            return db.Providers.Select(x =>
+           new SelectListItem { Text = x.Name, Value = x.Id.ToString() }).ToList();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,6 +59,14 @@ namespace Appsuggest.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ Appsuggest/Models/HomeSearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Appsuggest.Models
+{
+    public class HomeSearchViewModel
+    {
+        public string Search { get; set; }
+        public int? AppTypeId { get; set; }
+        public int? AppPlatformId { get; set; }
+        public int? ProviderId { get; set; }
+        public List<SelectListItem> AppTypeList { get; set; }
+        public List<SelectListItem> AppPlatformList { get; set; }
+        public List<SelectListItem> ProviderList { get; set; } 
+        public List<App> Apps { get; set; } 
+    }
+}
